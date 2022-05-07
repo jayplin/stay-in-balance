9b9e1b977156516ffbfee0a05f189ca0e2edf6dc
jayplin <jannik.schroeer@sap.com>
2022-05-07 07:56:42 +0100

    logo responsive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div flex flex-col h-full justify-end md:items-center text-primary sm:items-start font-decoy>
-    <img w-90 self-center src="/logo.svg">
+    <img w-50 md:w-90 self-center src="/logo.svg">
 
     <span pt-10 md:pt-20 text-2xl font-700 pb-3>
       Produktivität und Lebensqualität durch Yoga
