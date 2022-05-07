ede1de214e072a1433e42ce06f52573b0e25968c
jayplin <jannik.schroeer@sap.com>
2022-05-07 08:08:58 +0100

    landscape responsive

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main p="x-4 y-10" bg-brand-primary h-full>
+  <main p="x-4 y-10" bg-brand-primary md:h-full>
     <router-view />
   </main>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div flex flex-col h-full justify-end md:items-center text-primary sm:items-start font-decoy>
+  <div flex flex-col md:h-full justify-end md:items-center text-primary sm:items-start font-decoy>
     <img w-50 md:w-90 self-center src="/logo.svg">
 
     <span pt-10 md:pt-20 text-2xl font-700 pb-3>
